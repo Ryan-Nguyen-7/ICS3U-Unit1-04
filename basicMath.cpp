1f80a04c5b075c1dacebc499b301f5761fa7d8b9
Ubuntu <ubuntu@ip-172-31-40-214.ec2.internal>
2020-11-25 18:31:54 +0000

    added basicMath.CPP program

---
+++ basicMath.cpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2020 St. Mother Teresa HS All rights reserved
+//
+// created by Ryan Nguyen
+// Created on November 2020
+// This program calculates the area and perimeter of a rectangle
+//     with dimensions 5cm x 3cm
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions" << std::endl;
+    std::cout << "5cm x 3cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is " << (3*5) << "cm^2" << std::endl;
+    std::cout << "Perimeter is " << (2*(3+5)) << "cm" << std::endl;
+}
